9f04583480b906a116c18ca3295bbaca7b66f52d
Avinash Joshi <130389178+Avinash-2803@users.noreply.github.com>
2025-05-19 18:13:23 +0530

    Update workoutManager.js

--- src/Components/Home/workoutManager.js
+++ src/Components/Home/workoutManager.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const API_BASE_URL = "http://localhost:8000/api/user";
+const API_BASE_URL = "https://backend-3-unh6.onrender.com";
 
 const WorkoutManager = () => {
   const [workouts, setWorkouts] = useState([]);
